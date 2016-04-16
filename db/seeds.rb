3079bd67d300a69bb32b83fbb762f5b8a6e57cdd
Henrique Breim <breim@protonmail.ch>
2016-04-16 17:53:06 -0300

    Create seeds to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+	Page.create(title: 'O valor da página é', number: Random.rand(999))
+end
